27ec98d4e7a8dbbc0c79f04377e2512c379a17dd
frukttime <148493244+frukttime@users.noreply.github.com>
2024-10-07 19:12:00 +0300

    commit

--- data.js
+++ data.js
@@ -1,8 +1,8 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "0-6",
-      "name": "6",
+      "id": "0-tagaaed_3",
+      "name": "Tagaaed_3",
       "levels": [
         {
           "tileSize": 256,
@@ -33,32 +33,32 @@ var APP_DATA = {
           "yaw": 0.6195937736849153,
           "pitch": 0.14316303342259218,
           "rotation": 0,
-          "target": "1-7"
+          "target": "1-eesaed-vasakule"
         },
         {
           "yaw": 2.8642475618221575,
           "pitch": 0.07648351510395912,
           "rotation": 0,
-          "target": "6-5"
+          "target": "6-tagaaed_2"
         },
         {
           "yaw": -2.5119973118400196,
           "pitch": 0.06201488747940154,
           "rotation": 0,
-          "target": "5-4"
+          "target": "5-tagaaed_1"
         },
         {
           "yaw": -1.2689889646038583,
           "pitch": 0.0413005829543458,
           "rotation": 0,
-          "target": "4-3"
+          "target": "4-tagaaed"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "1-7",
-      "name": "7",
+      "id": "1-eesaed-vasakule",
+      "name": "Eesaed vasakule",
       "levels": [
         {
           "tileSize": 256,
@@ -89,20 +89,20 @@ var APP_DATA = {
           "yaw": -0.6166871489573911,
           "pitch": 0.07222087843753222,
           "rotation": 0,
-          "target": "2-1"
+          "target": "2-eesaed"
         },
         {
           "yaw": -2.6296658454164614,
           "pitch": 0.08120549026861923,
           "rotation": 0,
-          "target": "0-6"
+          "target": "0-tagaaed_3"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "2-1",
-      "name": "1",
+      "id": "2-eesaed",
+      "name": "Eesaed",
       "levels": [
         {
           "tileSize": 256,
@@ -133,20 +133,20 @@ var APP_DATA = {
           "yaw": 2.516658061307041,
           "pitch": 0.09208174232518473,
           "rotation": 0,
-          "target": "1-7"
+          "target": "1-eesaed-vasakule"
         },
         {
           "yaw": -1.3865646752088914,
           "pitch": 0.11389636862627839,
           "rotation": 0,
-          "target": "3-2"
+          "target": "3-eesaed-paremal"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "3-2",
-      "name": "2",
+      "id": "3-eesaed-paremal",
+      "name": "Eesaed paremal",
       "levels": [
         {
           "tileSize": 256,
@@ -177,20 +177,20 @@ var APP_DATA = {
           "yaw": -3.0430850369520357,
           "pitch": 0.06169029428705919,
           "rotation": 0,
-          "target": "4-3"
+          "target": "4-tagaaed"
         },
         {
           "yaw": 1.5612857812035728,
           "pitch": 0.09880421637560133,
           "rotation": 0,
-          "target": "2-1"
+          "target": "2-eesaed"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "4-3",
-      "name": "3",
+      "id": "4-tagaaed",
+      "name": "Tagaaed",
       "levels": [
         {
           "tileSize": 256,
@@ -221,26 +221,26 @@ var APP_DATA = {
           "yaw": 0.10959688639685439,
           "pitch": 0.09568002818845756,
           "rotation": 0,
-          "target": "3-2"
+          "target": "3-eesaed-paremal"
         },
         {
           "yaw": 1.8949777206379101,
           "pitch": 0.09770305024372838,
           "rotation": 0,
-          "target": "0-6"
+          "target": "0-tagaaed_3"
         },
         {
           "yaw": -3.059032036352969,
           "pitch": 0.0390723479415751,
           "rotation": 0,
-          "target": "5-4"
+          "target": "5-tagaaed_1"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "5-4",
-      "name": "4",
+      "id": "5-tagaaed_1",
+      "name": "Tagaaed_1",
       "levels": [
         {
           "tileSize": 256,
@@ -271,26 +271,26 @@ var APP_DATA = {
           "yaw": 0.6756515936332121,
           "pitch": 0.060328428835674686,
           "rotation": 0,
-          "target": "0-6"
+          "target": "0-tagaaed_3"
         },
         {
           "yaw": -0.035542563060397825,
           "pitch": 0.06113741003483497,
           "rotation": 0,
-          "target": "4-3"
+          "target": "4-tagaaed"
         },
         {
           "yaw": 1.5609661003832542,
           "pitch": 0.052966101767603746,
           "rotation": 0,
-          "target": "6-5"
+          "target": "6-tagaaed_2"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "6-5",
-      "name": "5",
+      "id": "6-tagaaed_2",
+      "name": "Tagaaed_2",
       "levels": [
         {
           "tileSize": 256,
@@ -321,23 +321,23 @@ var APP_DATA = {
           "yaw": -1.83623170610087,
           "pitch": 0.06856298398398408,
           "rotation": 0,
-          "target": "5-4"
+          "target": "5-tagaaed_1"
         },
         {
           "yaw": -0.13527519237507946,
           "pitch": 0.08745926165297924,
           "rotation": 0,
-          "target": "0-6"
+          "target": "0-tagaaed_3"
         }
       ],
       "infoHotspots": []
     }
   ],
-  "name": "Project Title",
+  "name": "Hindreku",
   "settings": {
     "mouseViewMode": "drag",
     "autorotateEnabled": false,
     "fullscreenButton": false,
-    "viewControlButtons": false
+    "viewControlButtons": true
   }
 };
